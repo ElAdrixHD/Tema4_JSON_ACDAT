aa60d2f4eea639f3423023868b99689aa6bb96f9
Adrian Muñoz <eladrixhd@gmail.com>
2019-01-10 20:53:49 +0100

    Commit para seguir en otro lugar de trabajo

--- app/src/main/java/adrianmmudarra/es/tema4_json_acdat/ui/MeteoActivity.java
+++ app/src/main/java/adrianmmudarra/es/tema4_json_acdat/ui/MeteoActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import adrianmmudarra.es.tema4_json_acdat.R;
 
 public class MeteoActivity extends AppCompatActivity {
+//https://stackoverflow.com/questions/3371326/java-date-from-unix-timestamp
+    private static final String URL = "http://api.openweathermap.org/data/2.5/weather?q=madrid,es&units=metric&APPID=8c8452ba700cbab5cd5c81d6cb3dd338";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
