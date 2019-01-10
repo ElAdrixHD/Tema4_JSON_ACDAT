89080d88c90e27d6aad65ea73766944694ecb112
Adrian Muñoz <eladrixhd@gmail.com>
2019-01-10 19:39:20 +0100

    Empezando proyecto

---
+++ app/src/main/java/adrianmmudarra/es/tema4_json_acdat/MenuActivity.java
@@ -0,0 +1,13 @@
+package adrianmmudarra.es.tema4_json_acdat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
