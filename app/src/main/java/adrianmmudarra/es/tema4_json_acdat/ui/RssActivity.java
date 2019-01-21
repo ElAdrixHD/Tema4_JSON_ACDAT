1daf22047b92d52dee6e46fe19ac1004c12042b2
Adrián Muñoz Mudarra <eladrixhd@gmail.com>
2019-01-21 12:10:11 +0100

    añadido links en comentarios para su futura implementacion

--- app/src/main/java/adrianmmudarra/es/tema4_json_acdat/ui/RssActivity.java
+++ app/src/main/java/adrianmmudarra/es/tema4_json_acdat/ui/RssActivity.java
@@ -7,6 +7,21 @@ import adrianmmudarra.es.tema4_json_acdat.R;
 
 public class RssActivity extends AppCompatActivity {
 
+    /*
+    https://www.abc.es/rss/feeds/abc_ultima.xml
+    https://www.europapress.es/rss/rss.aspx
+    https://www.20minutos.es/rss/
+    http://api2.rtve.es/rss/temas_noticias.xml
+    http://ep00.epimg.net/rss/tags/ultimas_noticias.xml
+    http://feeds2.feedburner.com/libertaddigital/portada
+    https://www.lavozdigital.es/rss/feeds/lavozdigital_ultima.xml
+    https://rss.elconfidencial.com/espana/
+    https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml
+    https://e00-expansion.uecdn.es/rss/portada.xml
+    https://as.com/rss/tags/ultimas_noticias.xml
+    https://www.elperiodico.com/es/rss/internacional/rss.xml
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
